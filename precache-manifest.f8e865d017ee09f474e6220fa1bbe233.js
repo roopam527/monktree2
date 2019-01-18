4e807dbed99fb4b964f7ad17c7734a8710131a2b
roopam527 <roopamg777@gmail.com>
2019-01-18 05:36:24 +0530

    'monktree-final

---
+++ precache-manifest.f8e865d017ee09f474e6220fa1bbe233.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9477bf20bcbc51c17e323dee3b728b8",
+    "url": "/static/media/Sign_up.c9477bf2.png"
+  },
+  {
+    "revision": "b4ba6d020b2b9a55f670",
+    "url": "/static/css/main.58cdfc8f.chunk.css"
+  },
+  {
+    "revision": "3c965f572a88ff53df73d03c8bef9559",
+    "url": "/static/media/profile.3c965f57.jpg"
+  },
+  {
+    "revision": "ef6c31e61778dd8dfebe",
+    "url": "/static/js/1.ef6c31e6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b4ba6d020b2b9a55f670",
+    "url": "/static/js/main.b4ba6d02.chunk.js"
+  },
+  {
+    "revision": "6e2b8906523d24c918183f5399b3cff6",
+    "url": "/static/media/Sign_in.6e2b8906.png"
+  },
+  {
+    "revision": "9e3fba5da79d3eef56bc988c456eb39c",
+    "url": "/static/media/home3.9e3fba5d.png"
+  },
+  {
+    "revision": "98b20889c972aa4c93b47e4a73e6e69e",
+    "url": "/static/media/backpack.98b20889.svg"
+  },
+  {
+    "revision": "ef6c31e61778dd8dfebe",
+    "url": "/static/css/1.5381ebe4.chunk.css"
+  },
+  {
+    "revision": "f2f12cb6272d625f81502ce6c82400cb",
+    "url": "/index.html"
+  }
+];
